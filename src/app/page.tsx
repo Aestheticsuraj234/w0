07e8e614a9730385fa92c82669a065a1b0797364
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-06-28 10:45:18 +0530

    04 Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query'
+import React from 'react'
 
 const Page = () => {
-  const trpc = useTRPC();
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  return (
+   <div className='p-4 max-w-7xl mx-auto'>
+      <Button onClick={()=>invoke.mutate({text:"Jhon"})}>
+        Invoke Background Job
+      </Button>
+   </div>
+  )
+}
 
-  const {data} = useQuery(trpc.hello.queryOptions({text: "world"}))
-
-  return <div className="font-bold">
-    {JSON.stringify(data)}
-  </div>;
-};
-
-export default Page;
+export default Page
